4afc62675c906a9ac838ec9c7f8f62724c360d99
Jonathan Schooler <violinplaya@gmail.com>
2012-06-05 08:46:20 -0700

    added frame support to cube

--- LED Cube/src/com/schooler/ledcube/model/Cube.java
+++ LED Cube/src/com/schooler/ledcube/model/Cube.java
@@ -1,16 +1,18 @@
 package com.schooler.ledcube.model;
 
-
 public class Cube {
 
 	public static final int DEFAULT_DIM = 8;
+	public static final int DEFAULT_FRAME_COUNT = 64;
 
-	private int dim;
-	private byte[] data;
+	private final int dim;
+	private byte[][] frames;
+	private int frameIdx;
 
 	private Cube(int dim) {
 		this.dim = dim;
-		data = new byte[dim * dim];
+		this.frameIdx = 0;
+		this.frames = new byte[DEFAULT_FRAME_COUNT][dim * dim];
 	}
 
 	public Cube() {
@@ -25,23 +27,39 @@ public class Cube {
 		return this.dim;
 	}
 
+	public int getFrameIndex() {
+		return frameIdx;
+	}
+
+	public void setFrameIndex(int frame) {
+		this.frameIdx = frame;
+	}
+
 	private int getIdx(int y, int z) {
 		return getIdx(0, y, z);
 	}
 
 	public byte[] getRawCube() {
-		return data;
+		return frames[frameIdx];
 	}
 
+	private byte[] getFrameBytes() {
+		return frames[frameIdx];
+	}
+
+	@SuppressWarnings("unused")
 	private void set(int y, int z, byte value) {
 		int idx = getIdx(y, z);
 
+		byte[] data = getFrameBytes();
 		byte before = data[idx];
 		data[idx] = value;
 	}
 
 	public void set(int x, int y, int z, boolean on) {
 		int idx = z * dim + y;
+
+		byte[] data = getFrameBytes();
 
 		byte before = data[idx];
 		if (on) {
@@ -53,10 +71,12 @@ public class Cube {
 
 	public boolean get(int x, int y, int z) {
 		int idx = z * dim + y;
+		byte[] data = getFrameBytes();
 		return (data[idx] & (1 << x)) > 0;
 	}
 
 	public byte getByte(int y, int z) {
+		byte[] data = getFrameBytes();
 		return data[getIdx(y, z)];
 	}
 }
